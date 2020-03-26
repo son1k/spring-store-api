27b99ffc289c9584807304ea483f80e2489ff40b
marciojava-prog <marciooliveirasantos2012@gmail.com>
2020-03-26 15:39:29 -0300

    alterando o banco de dados para mysql

--- ecommerce/src/main/java/br/com/ecommerce/ecommerce/repository/EstoqueRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.ecommerce.ecommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.ecommerce.ecommerce.domain.produtos.Estoque;
-
-public interface EstoqueRepository extends JpaRepository<Estoque, Integer>{
-
-}
--- ecommerce/src/main/java/br/com/ecommerce/ecommerce/repository/PagamentoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.ecommerce.ecommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.ecommerce.ecommerce.domain.pagamentos.Pagamento;
-
-public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>{
-
-}
